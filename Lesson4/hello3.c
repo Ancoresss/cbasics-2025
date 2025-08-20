d471f862491cfa256a3e76b4d342e340e41802e4
Eldar3502-hellcat <eldarangelov15@gmail.com>
2025-08-20 18:03:39 +0300

    Lesson4

---
+++ Lesson4/hello3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+//The first programm
+int main(){
+
+	int a;
+	int b;
+	printf("gimme first number:");
+	scanf("%d", &a);
+	printf("gimme second number:");
+	scanf("%d", &b);
+	printf("This is ur final result: %d\n", a*b);
+	printf("Good....by the way, take a coffee)\n");
+	return 0;
+}
