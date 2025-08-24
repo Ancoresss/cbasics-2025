10600131183ab7ad47cc3aa38fb024acb630c8c6
0vqo <anri36504@gmail.com>
2025-08-24 11:22:36 +0200

    rewrote lesson5 and added lesson6

---
+++ lesson6/multiplicationtable.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int i; 
+    
+    int number = 10;
+
+    for (i=1; i < 11; i++) {
+	    printf("%d\n", number * i);
+    }
+return 0;
+    
+       
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
