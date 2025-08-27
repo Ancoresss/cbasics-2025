d2960d643c8955da55fff4015258a65c43653ec0
FAvn1ch <toping35wer@gmail.com>
2025-08-27 17:54:56 +0300

    homework lesson 7

---
+++ lesson7/homework7.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main (){
+
+	int n;
+	printf("Enter the height: ");
+	scanf("%d", &n );
+
+	for (int i = 1; i <= n; i++);
+	for (int j = 0; j < n - i; j++) {
+		printf(" ");
+	}
+	
+	for (int a = 0; a , 2 * i - 1; a++){
+		printf("*");
+	}
+
+	printf("\n");
+}
+
+	int trunk_height = 2;
+	for (int i = 0; i < trunk_height; i++) {
+		for(int j = 0; j < n - 1; j++) {
+			printf(" ");
+		}
+		printf("|\n")
+	}
+
+	return 0;
+}
+
