ab1ed2a31d78d209bfe809fa303fff5854a09b99
Tryingspot <romaderkach2103@gmail.com>
2025-08-28 15:52:42 +0300

    exersices lesson7

---
+++ lesson3/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int number;
+
+  printf("enter the age");
+  scanf("%d", &number);
+  printf("you entered: %d\n", number);
+
+  return 0;
+}
