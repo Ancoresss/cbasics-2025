0831ea847c0b3e4baa80fa9bc420e160af8939c9
0vqo <anri36504@gmail.com>
2025-08-30 10:20:49 +0200

    homework

---
+++ lesson8/reverse.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+
+int numbers[5] = {};
+printf("enter 5 numbers: ");
+scanf("%d", &numbers[0]);
+scanf("%d", &numbers[1]);
+scanf("%d", &numbers[2]);
+scanf("%d", &numbers[3]);
+scanf("%d", &numbers[4]);
+
+for (int i = 5 - 1; i >= 0; i--) {
+	printf("%d\n", numbers[i]);
+}
+
+
+
+
+
+	return 0;
+}
